58129d3f6414d53cec8b73fb9f02883253f85f95
amitzed <amitzmn@gmail.com>
2020-10-28 17:33:57 -0400

    JSON api data successfully showing up on React front-end

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
+import trivia_data from '../apis/trivia_data';
+
 class App extends Component {
+  state = {
+    details: []
+  }
+
+  componentDidMount() {
+    trivia_data.get('/trivia-data')
+    .then((res) => {
+      const details = res.data;
+
+      this.setState({
+        details
+      })
+      console.log(this.state.details);
+    })
+    .catch((err) => {
+      console.error('Something Went Wrong');
+      console.error(err);
+    })
+  }
+
   render() {
+    const { details } = this.state;
+
     return (
       <div>
-        app
+        {details.map((detail) => (
+          <div key={detail.id}>
+            <h2>{detail.question}</h2>
+            <h4>ID: {detail.id}</h4>
+          </div>
+        ))}
       </div>
     )
   }
