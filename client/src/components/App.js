d96a53804b0044007f92c1c143c4c925e72e95d9
amitzed <amitzmn@gmail.com>
2020-10-28 21:20:34 -0400

    Data displaying on client and questions randomizing in console.log

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,18 +4,21 @@ import trivia_data from '../apis/trivia_data';
 
 class App extends Component {
   state = {
-    details: []
+    dataBank: []
   }
 
   componentDidMount() {
     trivia_data.get('/trivia-data')
     .then((res) => {
-      const details = res.data;
+      const dataBank = res.data;
 
       this.setState({
-        details
+        dataBank
       })
-      console.log(this.state.details);
+      // console.log(this.state.dataBank);
+      // Below picks 10 random questions out of 21
+      console.log(this.state.dataBank.sort(() => 0.5 - Math.random()).slice(0, 10));
+
     })
     .catch((err) => {
       console.error('Something Went Wrong');
@@ -24,14 +27,28 @@ class App extends Component {
   }
 
   render() {
-    const { details } = this.state;
+    const { dataBank } = this.state;
 
     return (
       <div>
-        {details.map((detail) => (
-          <div key={detail.id}>
-            <h2>{detail.question}</h2>
-            <h4>ID: {detail.id}</h4>
+        {dataBank.map((bank) => (
+          <div key={bank.id}>
+
+            <div>
+              <h4>Question #</h4>
+            </div>
+
+            <div>
+              <h4>{bank.question}</h4>
+            </div>
+
+            <div>
+              <button>{bank.answers[0]}</button>
+              <button>{bank.answers[1]}</button>
+              <button>{bank.answers[2]}</button>
+              <button>{bank.answers[3]}</button>
+            </div>
+
           </div>
         ))}
       </div>
