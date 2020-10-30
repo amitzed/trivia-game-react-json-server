41d2a3eb7daed2fd8f714a19f2c04b09a8096741
amitzed <amitzmn@gmail.com>
2020-10-29 22:04:54 -0400

    App successfully functioning and main styling applied

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
+import './App.css';
 import trivia_data from '../apis/trivia_data';
 
 const App = () => {
@@ -16,7 +17,6 @@ const App = () => {
       const dataBank = response.data;
       // Below picks 10 random questions out of 21: Math.random() returns values between 0 and .0999, so subtracting from 0.5 expands that range. After the array of questions is randomly sorted, the slice() method is used to pull only 10 items out of the randomized array (Which is considered one Round of questions)[from the first array index location(0) to the 10th index location(9) - 10 is written in the slice method because this method goes up to the 10th index but does not include it]
       setDetails(dataBank.sort(() => 0.5 - Math.random()).slice(0, 10));
-      // console.log(details.options.correct);
 
       // console.log(response.data.sort(() => 0.5 - Math.random()).slice(0, 10));
       // console.log(dataBank.length);
@@ -26,19 +26,28 @@ const App = () => {
   }, [])
 
   const [score, setScore] = useState(0);
-  const [viewScore, setViewScore] = useState(false);
+  const [scoreWindow, setScoreWindow] = useState(false);
   const [focusQuestion, setFocusQuestion] = useState(0);
 
   const userAnswerSelect = (correct) => {
+    if(!correct) {
+      // Show Correct answer on submit
+      alert(`Sorry, the correct answer is: ${details[focusQuestion].correct_answer}`)
+    }
+
     if(correct) {
       setScore(score + 1);
+      // Show Correct answer on submit
+      alert(`CORRECT! ${details[focusQuestion].correct_answer} is the right answer!`)
     }
 
     const next = focusQuestion + 1;
     if(next < details.length) {
+      // console.log(next);
+      // console.log(details.length);
       setFocusQuestion(next)
     } else {
-      setViewScore(true)
+      setScoreWindow(true)
     }
   }
 
@@ -46,16 +55,20 @@ const App = () => {
     window.location.reload(false);
   }
 
+  // If statement below is to prevent details below from throwing undefined error when tryiong to show questions individually without using map() method -- such as using {details[focusQuestion].question} OR {details[1].question}
+  if (!details.length) {
+    return null;
+  }
+
   return (
 
     <div className="game-container">
 
       {
-        viewScore
+        scoreWindow
         ?
         (
-
-          <div className="ui placeholder segment">
+          <div className="ui placeholder segment score-page-wrapper">
             <div className="ui two column stackable center aligned grid">
               <div className="ui vertical divider">
                 <i className="arrow circle right icon"></i>
@@ -70,7 +83,8 @@ const App = () => {
                 <div className="column">
                   <div className="ui icon header">
                     <i className="clipboard list icon"></i>
-                    Percentage: {score / details.length + '00'}%
+                    {/* Below toFixed() used to limit to two decimal places */}
+                    Percentage: {(score / details.length).toFixed(2) * 100}%
                   </div>
                   <button onClick={refreshPage} className="ui primary button">
                     Play Again
@@ -79,50 +93,33 @@ const App = () => {
               </div>
             </div>
           </div>
-
-
         )
         :
         (
           <div>
             <h2 className="ui icon header">
-              <i className="circular magic icon"></i>
+              <i className="circular magic icon icon-spin"></i>
               <div className="content">
                 Quizzard of Oz
-                <div className="sub header">Enter some extra text here</div>
+                <div className="question-element-wrapper">
+                  <div className="question-number">
+                    <span>Question {focusQuestion + 1} of {details.length}</span>
+                  </div>
+
+                  <div className="question-data">
+                    <h4>{details[focusQuestion].question}</h4>
+                  </div>
+                </div>
               </div>
             </h2>
 
-            <div>
-              Question #{focusQuestion} out of {details.length}
-            </div>
-
-            {details.map(detail => (
-              <div key={detail.id}>
-                <div>
-                  <h4>Question #</h4>
-                </div>
-
-                <div>
-                  <h4>{detail.question}</h4>
-                </div>
-
-                {detail.options.map((option, index) => (
-                  <div key={index}>
-                    <button onClick={() => userAnswerSelect(option.correct)}>{option.answer}</button>
-                      <span>
-                        {option.correct
-                        ?
-                        <span>True</span>
-                        :
-                        <span>False</span>
-                        }
-                      </span>
-                  </div>
+            <React.Fragment>
+              <div className="multiple-choice-options">
+                {details[focusQuestion].options.map((option, index) => (
+                  <button className="answer-options-btn" onClick={() => userAnswerSelect(option.correct)} key={index}>{option.answer}</button>
                 ))}
-
               </div>
-            ))}
+            </React.Fragment>
           </div>
         )
       }
